24d7c75f6e9e8d7b7e00a2e7c31773dac5db4917
DimitryRukhadze <lokiinflames@gmail.com>
2024-01-03 15:46:22 +0300

    Refactored admin

--- apps/tasks/admin.py
+++ apps/tasks/admin.py
@@ -4,11 +4,20 @@ from .models import Task, CW
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
+    list_display = ('code', 'task_description', 'is_deleted', )
     list_filter = ("is_deleted",)
-    search_fields = ("code__startswith",)
+    search_fields = ("code",)
+    ordering = ['code']
+
+    def task_description(self, obj):
+        return obj.description[:50]
+
+    task_description.short_description = 'Описание'
 
 
 @admin.register(CW)
 class CWAdmin(admin.ModelAdmin):
+    list_display = ("task", "perform_date", "next_due_date", "is_deleted")
     list_filter = ("is_deleted",)
-    search_fields = ("perform_date", "next_due_date")
+    search_fields = ("task", )
+    ordering = ['-perform_date']
