f3a01632dd50fbca7d9ea7a63fabfbc788f9b87f
DimitryRukhadze <lokiinflames@gmail.com>
2024-01-04 16:00:49 +0300

    Added API dummy

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -1,12 +1,31 @@
 from django.urls import path
-from ninja import NinjaAPI
+from django.shortcuts import get_object_or_404
+from ninja import NinjaAPI, Schema
+from typing import List
+
+from apps.tasks.models import Task
+
 
 api = NinjaAPI()
 
 
-@api.get("/add")
-def add(request, a: int, b: int):
-    return {"result": a + b}
+class TaskOut(Schema):
+    id: int
+    code: str
+    description: str
+    due_month: int = None
+
+
+@api.get("/tasks", response=List[TaskOut])
+def get_all_task(request):
+    tasks = Task.objects.active()
+    return tasks
+
+
+@api.get("/tasks/{task_id}", response=TaskOut)
+def get_task(request, task_id: int):
+    task = get_object_or_404("Task", id=task_id)
+    return task
 
 
 urlpatterns = [
